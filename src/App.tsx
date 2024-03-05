7fe1d1b27f52cae3bce5a89237ddf674c1afc21b
Olga_Belaya <protenot@yandex.ru>
2024-03-05 15:47:27 +0300

    add editorComponent

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,25 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { useState } from 'react';
+//import reactLogo from './assets/react.svg';
+//import viteLogo from '/vite.svg';
+import CodeEditorWindow from './components/CodeEditor';
 import './App.css'
+
 
 function App() {
   const [count, setCount] = useState(0)
-
+  const handleChange = (type: string, value: string) => {
+    console.log(`Change detected: ${type} - ${value}`);
+  };
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
+      
       <h1>Vite + React</h1>
+      <CodeEditorWindow
+        language="javascript"
+        theme="vs-dark"
+        code="// Your code here..."
+        onChange={handleChange}
+      />
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
---
+++ src/components/CodeEditor.tsx
@@ -0,0 +1,41 @@
+import React, { useState } from "react";
+import Editor from "@monaco-editor/react";
+
+interface CodeEditorWindowProps {
+  onChange: (type: string, value: string) => void;
+  language?: string;
+  code?: string;
+  theme?: string;
+}
+
+const CodeEditorWindow: React.FC<CodeEditorWindowProps> = ({
+  onChange,
+  language,
+  code,
+  theme
+}) => {
+  const [value, setValue] = useState(code || "");
+
+  const handleEditorChange = (newValue: string | undefined) => {
+    setValue(newValue || "");
+    setTimeout(() => {
+      onChange("code", newValue || "");
+    }, 500);
+  };
+
+  return (
+    <div className="overlay rounded-md overflow-hidden w-full h-full shadow-4xl">
+      <Editor
+        height="85vh"
+        width={`100%`}
+        language={language || "javascript"}
+        value={value}
+        theme={theme || "vs-dark"}
+        onChange={handleEditorChange}
+      />
+    </div>
+  );
+};
+
+export default CodeEditorWindow;
+

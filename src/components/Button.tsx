40f434ed17f95a22574db1611c8497e2de537223
Olga_Belaya <protenot@yandex.ru>
2024-03-06 21:03:30 +0300

    add compare button functional

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 interface ButtonProps {
 text:string,
-onClick:()=>void;
+onClick:(arg?:string)=>void;
 }
 
 const Button:React.FC<ButtonProps> = ({text,onClick})=>{
--- src/components/Task.tsx
+++ src/components/Task.tsx
@@ -1,22 +1,53 @@
-import React from 'react';
+import React, {useState} from 'react';
 import Button from './Button'
 import CodeEditorWindow from './CodeEditor';
-function Task () {
-
+import {TASKS} from '../fakeDB/tasks';
+function Task () {0
+    const [theme, setTheme]=useState('vs-dark');
+    const [solution, setSolution] = useState('');
+    const [information,setInformation] = useState('')
     const handleChange = (type: string, value: string) => {
         console.log(`Change detected: ${type} - ${value}`);
-      };
+    if(type==='code'){
+        setSolution(value)
+    }  
+    
+    };
       const handleClickBack = ()=>{
         console.log("Здесь будет логика возвращения на страницу выбора задач")
+      }
+
+      const handleClickTheme = ()=>{
+        const newTheme=theme==='vs-dark'?'vs-light':'vs-dark';
+        setTheme(newTheme)
+      }
+        
+      const handleClickCompare=(taskId:string)=>{
+        const  task = TASKS.find(task =>task.id===taskId)
+        if(task){
+            const standartSolution = task.solution;
+            if(solution === standartSolution){
+                setInformation("Вы правильно решили задачу");
+            } else{
+                setInformation("Попробуйте еще раз")
+            }
+        }
+
+
       }
   return(<div>
     <Button text ="Вернуться к выбору задачи" onClick = {handleClickBack}/>
     <CodeEditorWindow
         language="javascript"
-        theme="vs-dark"
+        theme={theme}
         code="// Your code here..."
         onChange={handleChange}
+        setSolution={setSolution}
+        
       />
+      <Button text ="Выберете тему" onClick = {handleClickTheme}/>
+      <Button text ="Отправить на проверку" onClick = {()=>handleClickCompare("12345")}/>
+    <p>{information}</p>
     </div>)
 }
 export default Task
